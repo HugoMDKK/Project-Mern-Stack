17a417c9c5bf21c7087c1ee35eb23e9082006c32
Victor Hugo <victorhugo0856@gmail.com>
2025-02-09 17:49:18 -0300

    feat: add create news

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import connectDatabase from "./src/database/db.js";
 
 import userRoute from "./src/routes/user.route.js";
 import authRoute from "./src/routes/auth.route.js";
+import newsRoute from "./src/routes/news.route.js";
 
 import 'dotenv/config'; // Formato ES Modules
 
@@ -11,9 +12,10 @@ const port = process.env.PORT || 3000;
 
 
 connectDatabase()
-app.use(express.json());
+app.use(express.json());s
 
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
+app.use("/news", newsRoute);
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
